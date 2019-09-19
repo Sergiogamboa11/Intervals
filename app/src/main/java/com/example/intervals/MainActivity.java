012749b2c147c6d5f336bf098cc52f22b09727b2
Sergiogamboa11 <sergio.gamboa11@gmail.com>
2019-09-18 21:31:56 -0600

    Changed icons and text for navigationView

--- app/src/main/java/com/example/intervals/MainActivity.java
+++ app/src/main/java/com/example/intervals/MainActivity.java
@@ -47,14 +47,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_stopwatch:
+                    mTextMessage.setText(R.string.title_stopwatch);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_presets:
+                    mTextMessage.setText(R.string.title_presets);
                     return true;
             }
             return false;
